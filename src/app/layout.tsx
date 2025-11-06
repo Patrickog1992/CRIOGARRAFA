a6788b8520559ce8eaa68c42d5f12fc9bf3b0f24
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-11-06 14:17:29 +0000

    FILHA DA PUTA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'CrioCaseira Quizz',
@@ -18,6 +19,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <Script src="https://scripts.converteai.net/82b0f5b7-3ef8-4fad-9a6a-1e700b3d750b/players/690b96ce1e2607a796dc4b67/v4/player.js" strategy="lazyOnload" />
+        <Script src="https://scripts.converteai.net/82b0f5b7-3ef8-4fad-9a6a-1e700b3d750b/players/690ba39e1e2607a796dc5d41/v4/player.js" strategy="lazyOnload" />
       </head>
       <body className="font-body antialiased">
         {children}
--- src/components/VturbPlayer.tsx
+++ src/components/VturbPlayer.tsx
@@ -1,30 +1,23 @@
 "use client";
-import Script from "next/script";
+
 import type { FC } from 'react';
 
 type VturbPlayerProps = {
   playerId: string;
 };
 
-const VTURB_USER_ID = "82b0f5b7-3ef8-4fad-9a6a-1e700b3d750b";
-
 export const VturbPlayer: FC<VturbPlayerProps> = ({ playerId }) => {
-  const playerSrc = `https://scripts.converteai.net/${VTURB_USER_ID}/players/${playerId}/v4/player.js`;
   const smartPlayerId = `vid-${playerId}`;
 
   return (
-    <>
       <div
+        id={smartPlayerId}
         style={{
           display: "block",
           margin: "0 auto",
           width: "100%",
-          maxWidth: "400px",
+          maxWidth: "800px",
         }}
-      >
-        <div id={smartPlayerId}></div>
-      </div>
-      <Script src={playerSrc} strategy="lazyOnload" />
-    </>
+      ></div>
   );
 };
