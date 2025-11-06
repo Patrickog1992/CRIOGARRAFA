67a7f0017a389210a1bbe3776fc5671129cba521
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-11-06 14:18:21 +0000

    o video está tomando toda a página faça ele caber na página

--- src/components/VturbPlayer.tsx
+++ src/components/VturbPlayer.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import type { FC } from 'react';
+import Script from 'next/script';
 
 type VturbPlayerProps = {
   playerId: string;
@@ -8,8 +9,11 @@ type VturbPlayerProps = {
 
 export const VturbPlayer: FC<VturbPlayerProps> = ({ playerId }) => {
   const smartPlayerId = `vid-${playerId}`;
+  const scriptSrc = `https://scripts.converteai.net/82b0f5b7-3ef8-4fad-9a6a-1e700b3d750b/players/${playerId}/v4/player.js`;
 
   return (
+    <>
+      <Script src={scriptSrc} strategy="lazyOnload" />
       <div
         id={smartPlayerId}
         style={{
@@ -19,5 +23,6 @@ export const VturbPlayer: FC<VturbPlayerProps> = ({ playerId }) => {
           maxWidth: "800px",
         }}
       ></div>
+    </>
   );
 };
